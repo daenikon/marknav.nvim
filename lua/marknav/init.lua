418e16212b1b1201e736a3aa423468b818ddfe89
daenikon <dkondratiuk06@gmail.com>
2024-01-19 12:41:26 +0100

    Add error handling and change command names
    - Print Error when:
        * Link is not found
        * File doesn't exist
        * Buffer is empty
    - Change command names from :MarkLink.. to :MarkNav..

--- lua/marknav/init.lua
+++ lua/marknav/init.lua
@@ -10,33 +10,47 @@ end
 
 -- Function to pop a buffer from the stack and switch to it
 local function pop_and_go_to_buffer()
-    if #buffer_stack > 0 then
-        local last_bufnr = table.remove(buffer_stack)
-        vim.api.nvim_command('buffer ' .. last_bufnr)
+    if #buffer_stack == 0 then
+        print("Buffer history is empty.")
+        return
     end
+
+    local last_bufnr = table.remove(buffer_stack)
+    if not vim.api.nvim_buf_is_loaded(last_bufnr) then
+        print("Previous buffer no longer exists.")
+        return
+    end
+
+    vim.api.nvim_command('buffer ' .. last_bufnr)
 end
 
 
--- Function to check if the cursor is on line with relative link and open the link
+
+-- Function to check if the cursor is on the relative link and open the link
 function M.check_cursor_on_link()
     local current_line = vim.api.nvim_get_current_line()
     local cursor_col = vim.api.nvim_win_get_cursor(0)[2] + 1
 
-    -- Check if the cursor is on a Markdown link (basic pattern matching)
     local link_start, link_end, link_path = string.find(current_line, '%[[^%]]+%]%(([^%)%]]*)%)')
-    if link_path and cursor_col >= link_start and cursor_col <= link_end then
-        push_buffer(vim.api.nvim_get_current_buf())
 
-        -- Get the directory of the current file
-        local current_file_dir = vim.fn.expand('%:p:h')
-
-        -- Resolve the absolute path of the link
-        local absolute_link_path = current_file_dir .. '/' .. link_path
-
-        -- Open the file at the resolved path
-        vim.api.nvim_command('edit ' .. absolute_link_path)
+    if not link_path or cursor_col < link_start or cursor_col > link_end then
+        print("No Markdown link found under cursor.")
+        return
     end
+
+    push_buffer(vim.api.nvim_get_current_buf())
+
+    local current_file_dir = vim.fn.expand('%:p:h')
+    local absolute_link_path = current_file_dir .. '/' .. link_path
+
+    if vim.fn.filereadable(absolute_link_path) == 0 then
+        print("The linked file does not exist: " .. absolute_link_path)
+        return
+    end
+
+    vim.api.nvim_command('edit ' .. absolute_link_path)
 end
+
 
 -- Function to go to the previous buffer in the stack
 function M.go_to_previous_buffer()
@@ -46,12 +60,12 @@ end
 -- Set up commands for Markdown file navigation
 function M.setup()
     vim.api.nvim_create_user_command(
-        'MarkLinkNext',
+        'MarkNavNext',
         M.check_cursor_on_link,
         {nargs = 0}
     )
     vim.api.nvim_create_user_command(
-        'MarkLinkPrevious',
+        'MarkNavPrevious',
         M.go_to_previous_buffer,
         {nargs = 0}
     )
